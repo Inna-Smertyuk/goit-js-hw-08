f810e8161bc5b3e7f074a3cdc66a76f0e83a7059
Inna-Smertyuk <86621124+Inna-Smertyuk@users.noreply.github.com>
2021-11-23 00:10:05 +0200

    hw-8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
+//import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const KEY_TIME = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+function getCurrentTime(time) {
+    const currentTime = time.seconds;
+
+    localStorage.setItem(KEY_TIME, currentTime);
+
+};
+
+player.setCurrentTime(saveTime()).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
+
+function saveTime() {
+    return Number(localStorage.getItem(KEY_TIME));
+}

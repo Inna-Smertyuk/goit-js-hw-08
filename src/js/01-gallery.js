16345f2363dd5fcdc460c890b2c318a94a3e64fb
Inna-Smertyuk <86621124+Inna-Smertyuk@users.noreply.github.com>
2021-11-22 22:08:55 +0200

    hw-8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryElements = document.querySelector(".gallery");
+let galleryMarkup = createMarkup(galleryItems);
+galleryElements.insertAdjacentHTML("beforeend", galleryMarkup);
 
+function createMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+    <a class="gallery__item" href="${original}" >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}" 
+    />
+  </a> `;
+    }).join('');
+};
+
+const lightbox = new SimpleLightbox('.gallery a', { captions: true, captionsData: 'alt', captionDelay: 250 })
 console.log(galleryItems);

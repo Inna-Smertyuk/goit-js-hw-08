31df16397f2b145fb1d295e1441bd0e5b5ddc32c
Inna-Smertyuk <86621124+Inna-Smertyuk@users.noreply.github.com>
2021-11-23 23:51:40 +0200

    hw-8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const formRef = document.querySelector('.feedback-form');
+const inputRef = document.querySelector('.feedback-form input');
+const textareaRef = document.querySelector('.feedback-form textarea');
+
+formRef.addEventListener('submit', onFormSubmit);
+formRef.addEventListener('input', throttle(onInput, 500));
+
+let formData = {
+    email: '',
+    message: '',
+};
+
+function onInput(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    console.log(formData);
+
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+
+    formData = {
+        email: '',
+        message: '',
+    }
+};
+
+function saveFormData() {
+    const savedInfo = localStorage.getItem(STORAGE_KEY);
+    if (savedInfo) {
+        const pasrsedSavedInfo = JSON.parse(savedInfo);
+
+        formData = pasrsedSavedInfo;
+
+        inputRef.value = pasrsedSavedInfo.email;
+        textareaRef.value = pasrsedSavedInfo.message;
+    }
+};
+saveFormData();
